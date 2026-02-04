41b22e06c91436efcc943c8cd8892e49b616a6dc
altorarreiss-source <altorarreiss@gmail.com>
2026-02-04 13:11:01 -0500

    Actualizando Precios.py

--- Precios.py
+++ Precios.py
@@ -1,2 +1,21 @@
+"""Esta clase contiene los productos y los precios, también permite acceder a los precios de los productos a través de el id"""
 class Precios:
-	pass
+    def __init__(self):
+        self._precios = {}
+        self._productos = {}
+    
+    def agregar_producto(self, producto, precio):
+        self._productos[producto.id] = producto
+        self._precios[producto.id] = precio
+	
+    def getPriceFrom(self, producto_id):
+        return self._precios.get(producto_id)
+    
+    def listar_productos(self):
+        return list(self._productos.values())
+    
+    def actualizar_precio(self, producto_id, nuevo_precio):
+        if producto_id in self._precios:
+            self._precios[producto_id] = nuevo_precio
+            return True
+        return False
